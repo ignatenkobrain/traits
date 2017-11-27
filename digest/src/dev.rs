8090d8272af740ea213bdcf99b97a2e747e27148
newpavlov <newpavlov@gmail.com>
2017-11-27 20:18:32 +0300

    added variable_test

--- digest/src/dev.rs
+++ digest/src/dev.rs
@@ -4,7 +4,7 @@ use core::fmt::Debug;
 
 #[macro_export]
 macro_rules! new_test {
-    ($name:ident, $test_name:expr, $hasher:ty, $test_func:expr) => {
+    ($name:ident, $test_name:expr, $hasher:ty, $test_func:ident) => {
         #[test]
         fn $name() {
             let inputs = include_bytes!(
@@ -16,7 +16,6 @@ macro_rules! new_test {
 
             // u16 (2 bytes); start + end (x2); input, output (x2)
             assert_eq!(index.len() % (2*2*2), 0, "invlaid index length");
-            let mut hasher = <$hasher>::default();
             for (i, chunk) in index.chunks(2*2*2).enumerate() {
                 // proper aligment is assumed here
                 let mut idx = unsafe {
@@ -29,7 +28,7 @@ macro_rules! new_test {
                 let input = &inputs[(idx[0][0] as usize)..(idx[0][1] as usize)];
                 let output = &outputs[
                     (idx[1][0] as usize)..(idx[1][1] as usize)];
-                if let Some(desc) = $test_func(&mut hasher, input, output) {
+                if let Some(desc) = $test_func::<$hasher>(input, output) {
                     panic!("\n\
                         Failed test №{}: {}\n\
                         input: [{}..{}]\t{:?}\n\
@@ -45,10 +44,11 @@ macro_rules! new_test {
     }
 }
 
-pub fn digest_test<D>(hasher: &mut D, input: &[u8], output: &[u8])
+pub fn digest_test<D>(input: &[u8], output: &[u8])
     -> Option<&'static str>
     where D: Digest + Debug + Clone
 {
+    let mut hasher = D::default();
     // Test that it works when accepting the message all at once
     hasher.input(input);
     if hasher.result().as_slice() != output {
@@ -83,10 +83,11 @@ pub fn digest_test<D>(hasher: &mut D, input: &[u8], output: &[u8])
     None
 }
 
-pub fn xof_test<D>(hasher: &mut D, input: &[u8], output: &[u8])
+pub fn xof_test<D>(input: &[u8], output: &[u8])
     -> Option<&'static str>
     where D: Input + ExtendableOutput + Default + Debug + Clone
 {
+    let mut hasher = D::default();
     let mut buf = [0u8; 1024];
     // Test that it works when accepting the message all at once
     hasher.process(input);
@@ -137,156 +138,42 @@ pub fn xof_test<D>(hasher: &mut D, input: &[u8], output: &[u8])
     None
 }
 
-/*
-#[derive(Debug, Clone)]
-struct Dummy;
-
-impl Input for Dummy {
-    fn process(&mut self, _: &[u8]) {}
-}
-
-use generic_array::GenericArray;
-use generic_array::typenum::U1;
-
-impl super::FixedOutput for Dummy {
-    type OutputSize = U1;
-    fn fixed_result(&mut self) -> GenericArray<u8, U1> {
-        Default::default()
-    }
-}
-
-new_test!(dummy_test, "dummy", Dummy, digest_test);
-
-*/
-/*
-
-pub struct Test {
-    pub name: &'static str,
-    pub input: &'static [u8],
-    pub output: &'static [u8],
-}
-
-#[macro_export]
-macro_rules! new_tests {
-    [ $( $name:expr ),*  ] => {
-        [$(
-            Test {
-                name: $name,
-                input: include_bytes!(concat!("data/", $name, ".input.bin")),
-                output: include_bytes!(concat!("data/", $name, ".output.bin")),
-            },
-        )*]
-    };
-    [ $( $name:expr ),+, ] => (new_tests![$($name),+])
-}
-
-pub fn run_digest_tests<D: Digest + Debug + Clone>(tests: &[Test]) {
-    // Test that it works when accepting the message all at once
-    for t in tests.iter() {
-        let mut sh = D::default();
-        sh.input(t.input);
-
-        let out = sh.result();
-
-        assert_eq!(out[..], t.output[..]);
-    }
-
-    // Test that it works when accepting the message in pieces
-    for t in tests.iter() {
-        let mut sh = D::default();
-        let len = t.input.len();
-        let mut left = len;
-        while left > 0 {
-            let take = (left + 1) / 2;
-            sh.input(&t.input[len - left..take + len - left]);
-            left = left - take;
-        }
-
-        let out = sh.result();
-
-        assert_eq!(out[..], t.output[..]);
-    }
-}
-
-pub fn run_variable_tests<D>(tests: &[Test])
-    where D: Input + VariableOutput + Clone + Debug
+pub fn variable_test<D>(input: &[u8], output: &[u8])
+    -> Option<&'static str>
+    where D: Input + VariableOutput + Debug + Clone
 {
-    let mut buf = [0u8; 1024];
+    let mut hasher = D::new(output.len()).unwrap();
+    let mut buf = [0u8; 128];
+    let buf = &mut buf[..output.len()];
     // Test that it works when accepting the message all at once
-    for t in tests.iter() {
-        let mut sh = D::new(t.output.len()).unwrap();
-        sh.process(t.input);
+    hasher.process(input);
+    hasher.variable_result(buf).unwrap();
+    if buf != output { return Some("whole message"); }
 
-        let out = sh.variable_result(&mut buf[..t.output.len()]).unwrap();
-
-        assert_eq!(out[..], t.output[..]);
-    }
+    // Test if reset works correctly
+    hasher.process(input);
+    hasher.variable_result(buf).unwrap();
+    if buf != output { return Some("whole message after reset"); }
 
     // Test that it works when accepting the message in pieces
-    for t in tests.iter() {
-        let mut sh = D::new(t.output.len()).unwrap();
-        let len = t.input.len();
-        let mut left = len;
-        while left > 0 {
-            let take = (left + 1) / 2;
-            sh.process(&t.input[len - left..take + len - left]);
-            left = left - take;
-        }
-
-        let out = sh.variable_result(&mut buf[..t.output.len()]).unwrap();
-
-        assert_eq!(out[..], t.output[..]);
+    let len = input.len();
+    let mut left = len;
+    while left > 0 {
+        let take = (left + 1) / 2;
+        hasher.process(&input[len - left..take + len - left]);
+        left = left - take;
     }
+    hasher.variable_result(buf).unwrap();
+    if buf != output { return Some("message in pieces"); }
+
+    // Test processing byte-by-byte
+    for chunk in input.chunks(1) {
+        hasher.process(chunk)
+    }
+    hasher.variable_result(buf).unwrap();
+    if buf != output { return Some("message byte-by-byte"); }
+    None
 }
-
-
-pub fn run_xof_tests<D>(tests: &[Test])
-    where D: Input + ExtendableOutput + Default + Debug + Clone
-{
-    let mut buf = [0u8; 1024];
-    // Test that it works when accepting the message all at once
-    for t in tests.iter() {
-        let mut sh = D::default();
-        sh.process(t.input);
-
-        let out = &mut buf[..t.output.len()];
-        sh.xof_result().read(out);
-
-        assert_eq!(out[..], t.output[..]);
-    }
-
-    // Test that it works when accepting the message in pieces
-    for t in tests.iter() {
-        let mut sh = D::default();
-        let len = t.input.len();
-        let mut left = len;
-        while left > 0 {
-            let take = (left + 1) / 2;
-            sh.process(&t.input[len - left..take + len - left]);
-            left = left - take;
-        }
-
-        let out = &mut buf[..t.output.len()];
-        sh.xof_result().read(out);
-
-        assert_eq!(out[..], t.output[..]);
-    }
-
-    // Test reeading from reader byte by byte
-    for t in tests.iter() {
-        let mut sh = D::default();
-        sh.process(t.input);
-
-        let mut reader = sh.xof_result();
-        let out = &mut buf[..t.output.len()];
-        for chunk in out.chunks_mut(1) {
-            reader.read(chunk);
-        }
-
-        assert_eq!(out[..], t.output[..]);
-    }
-}
-*/
 
 pub fn one_million_a<D>(expected: &[u8])
     where D: Digest + Default + Debug + Clone
